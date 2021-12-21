6e05283688ac0642d8b40f6ad30532d111bd1b08
mailahnheidi@gmail.com <mailahnheidi@gmail.com>
2021-12-21 12:46:14 -0600

    figured out local storage save and load. Rearranged some things in the html

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,60 +34,60 @@
 $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
 
-// content textarea was clicked
-$(".content").on("click", "p", function () {
-    //get current text of p element
-    var text = $(this)
-        .text()
-        .trim();
+// // content textarea was clicked
+// $(".content").on("click", "textarea", function () {
+//     //get current text of p element
+//     var text = $(this)
+//         .text()
+//         .trim();
 
-    //replace <p> with text area
-    var textInput = $("<textarea>").addClass(".form-control").val(text);
-    $(this).replaceWith(textInput);
+//     //replace <p> with text area
+//     var textInput = $("<textarea>").addClass(".form-control").val(text);
+//     $(this).replaceWith(textInput);
 
-    //aut focus on new element 
-    textInput.trigger("focus");
-});
+//     //aut focus on new element 
+//     textInput.trigger("focus");
+// });
 
 
-//editable field was un-focused
-$(".content").on("blur", "textarea", function () {
-    //get current value of textarea
-    var text = $(this).val();
+// //editable field was un-focused
+// $(".content").on("blur", "textarea", function () {
+//     //get current value of textarea
+//     var text = $(this).val();
 
-    // //get status type and position in the list 
-    // var status = $(this)
-    // .closest(".list-group")
-    // .attr("id")
-    // .replace("list-", "");
-    // var index = $(this)
-    // .closest(".list-group-item")
-    // .index();
+//     // //get status type and position in the list 
+//     // var status = $(this)
+//     // .closest(".list-group")
+//     // .attr("id")
+//     // .replace("list-", "");
+//     // var index = $(this)
+//     // .closest(".list-group-item")
+//     // .index();
 
-    // // update task in array and re-save to localstorage
-    // tasks[status][index].text = text;
-    // saveTasks(); 
+//     // // update task in array and re-save to localstorage
+//     // tasks[status][index].text = text;
+//     // saveTasks(); 
 
-    // recreate p element
-    var taskP = $("<p>")
-        .addClass("m-1")
-        .text(text);
+//     // recreate p element
+//     var taskP = $("<p>")
+//         .addClass("m-1 description")
+//         .text(text);
 
-    // replace textarea with new content
-    $(this).replaceWith(taskP);
-})
+//     // replace textarea with new content
+//     $(this).replaceWith(taskP);
+// })
 
 $(".saveBtn").on("click", function () {
     //get values and save
-    var taskP = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
+    var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
     var time = $(this).parent().attr("id"); // taken the change from the parent html id attribute
 
     //set items in local storage.
-    localStorage.setItem(time, taskP);
+    localStorage.setItem(time, text);
 })
 
 // Get item from local storage if any
-$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour8 .description").text(localStorage.getItem("hour8"));
 $("#hour9 .description").val(localStorage.getItem("hour9"));
 $("#hour10 .description").val(localStorage.getItem("hour10"));
 $("#hour11 .description").val(localStorage.getItem("hour11"));
